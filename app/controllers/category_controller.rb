af41648dd8477785ba896ef2efd0be1a0bc2731c
Ivan Pauls <ivanpauls1705@gmail.com>
2019-02-21 16:50:39 +0200

    Add scope Book

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,26 @@
+require 'pry'
 class CategoryController < ApplicationController
     def index
+      @categories = Category.all
+      @books = Book.all
+      @books_all = Book.all.count
+    end
+
+    def show
+      @books_all = Book.all.count
+      # @books_all = Book.all.count
+      @categories = Category.all
+      # if params[:id] == 'all'
+        @books = Book.price_asc.all
+      # else
+      #   @books = Book.where(category_id: params[:id])
+      # end
+      # make_selection
+    end
+
+    def make_selection
+      @selection = Book.where(nil)
+      @selection
+      binding.pry
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :authors
 
   belongs_to :category
+
+  scope :newest, -> () { order('created_at DESC') }
+  scope :popular_desc, -> () { order('used DESC') }
+  scope :price_desc, -> () { order('price DESC') }
+  scope :price_asc, -> () { order('price ASC') }
 end

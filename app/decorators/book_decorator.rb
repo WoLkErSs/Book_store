afcfd8961f727da65974cf65557c0f9d079d0fe0
Ivan Pauls <ivanpauls1705@gmail.com>
2019-03-04 22:57:58 +0200

    Change books show

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,8 +1,13 @@
 class BookDecorator < Draper::Decorator
   delegate_all
+  DESCRIPTION_IN_RANGE = 0..100
 
   def authors_all
     authors.map(&:name).join(', ')
+  end
+
+  def short_description
+    description[DESCRIPTION_IN_RANGE]
   end
 
 end
